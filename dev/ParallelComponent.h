c9b79f47e4df38a6c71e66240bd21342825a38e6
Harry Brenton <el22hjb@leeds.ac.uk>
2023-11-15 01:36:06 +0000

    parallel component now calculates parallel combos

--- dev/ParallelComponent.h
+++ dev/ParallelComponent.h
@@ -1,16 +1,18 @@
-#ifndef PARRALLEL_COMPONENT_H
-#define PARRALLEL_COMPONENT_H
+#ifndef PARALLEL_COMPONENT_H
+#define PARALLEL_COMPONENT_H
 
 #include "DefaultComponent.h"
 
-class ParrallelComponent : public DefaultComponent {
+class ParallelComponent : public DefaultComponent {
     public:
         // Constructor with default arguments - Values are passed as arrays
-        explicit ParrallelComponent(double resistors[] =  nullptr, double inductors[] = nullptr, double capacitors[] = nullptr);
+        explicit ParallelComponent(size_t sr = 0, double *resistors =  nullptr, 
+        size_t sl = 0, double *inductors = nullptr, 
+        size_t s = 0, double *capacitors = nullptr);
         // Mutators that calculate member variables based on Parallel circuit laws
-        void set_resistance(double resistors[]);
-        void set_inductance(double inductors[]);
-        void set_capacitance(double capacitors[]);
+        void set_resistance(size_t sr, double *resistors);
+        void set_inductance(size_t sl, double *inductors);
+        void set_capacitance(size_t sc, double *capacitors);
         // Accessors that return total R, L, C in parallel
         double get_resistance();
         double get_inductance();
--- dev/ParrallelComponent.cpp
+++ dev/ParrallelComponent.cpp
@@ -1,43 +1,41 @@
-#include "ParrallelComponent.h"
+#include "ParallelComponent.h"
 
-ParrallelComponent::ParrallelComponent(double resistors[], double inductors[], double capacitors[]) {
+ParallelComponent::ParallelComponent(size_t sr, double *resistors, size_t sl, double *inductors, size_t sc, double *capacitors) {
     // Calculate and set resistance
-    ParrallelComponent::set_resistance(resistors);
+    ParallelComponent::set_resistance(sr, resistors);
     // Calculate and set inductance
-    ParrallelComponent::set_inductance(inductors);
+    ParallelComponent::set_inductance(sl, inductors);
     // Calculate and set Capacitance
-    ParrallelComponent::set_capacitance(capacitors);
+    ParallelComponent::set_capacitance(sc, capacitors);
 }
 
-void ParrallelComponent::set_resistance(double resistors[]){
+void ParallelComponent::set_resistance(size_t sr, double *resistors){
     // Loop through array of resistors and combine into one "resistor"
     double combined_resistance = 0;
-    for (size_t i = 0; i < 20; i++)
-    {
-        /* code */
+    for (size_t i = 0; i < sr; i++) {
+        combined_resistance += 1 / *(resistors+i);
     }
-    
-    _R = combined_resistance;
+    _R = 1/combined_resistance;
 }
 
-void ParrallelComponent::set_inductance(double inductors[]){
+void ParallelComponent::set_inductance(size_t sl, double *inductors){
     // Loop through array of inductors and combine into one "inductor"
     double combined_inductance = 0;
-    for (size_t i = 0; i < 20; i++)
-    {
-        /* code */
+    for (size_t i = 0; i < sl; i++) {
+        combined_inductance += 1 / *(inductors+i);
     }
-    
-    _R = combined_inductance;
+    _L = 1/combined_inductance;
 }
 
-void ParrallelComponent::set_capacitance(double capacitors[]){
+void ParallelComponent::set_capacitance(size_t sc, double *capacitors){
     // Loop through array of capacitors and combine into one "capacitor"
     double combined_capacitance = 0;
-    for (size_t i = 0; i < 20; i++)
-    {
-        /* code */
+    for (size_t i = 0; i < sc; i++) {
+        combined_capacitance += 1 / *(capacitors+i);
     }
-    
-    _R = combined_capacitance;
+    _C = combined_capacitance;
 }
+
+double ParallelComponent::get_resistance() { return _R; }
+double ParallelComponent::get_inductance() { return _L; }
+double ParallelComponent::get_capacitance() { return _C; }
