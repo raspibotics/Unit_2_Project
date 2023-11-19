4d909dfec0ae70e017dcb2bc3d5e392d931ce93a
Harry Brenton <el22hjb@leeds.ac.uk>
2023-11-19 13:43:57 +0000

    moved parallelComponent into Components

--- dev/Solver.cpp
+++ dev/Solver.cpp
@@ -1,7 +1,21 @@
 // Takes a list of Components from Parser.cpp and models them as a laplace transformed polynomial
 #include <cmath>
+#include "Components.h"
+#include "ParallelComponent.h"
 
 
 int main(int argc, char *argv[]){
+    double a; 
+    double b;
+    double c; 
+    double vs[2]; 
+}
 
+void determine_coefficients(DefaultComponent components[]){
+    
+}
+
+double solve_diff_eq(double &a, double &b, double &c, double &vs){
+    double x; 
+    return x;
 }
--- dev/Sources.cpp
+++ dev/Sources.cpp
@@ -4,7 +4,7 @@
 DC_VoltageSource class definitions
 ------------------------------------------------------------------ */ 
 // Default constructor sets amplitude to 5.00V and impedence to 25Ω
-DC_VoltageSource::DC_VoltageSource() : DefaultComponent{}, _V{5.00}, _Z{25} {}
+DC_VoltageSource::DC_VoltageSource() : DefaultComponent{}, _V{5.00}, _Z{0} {}
 DC_VoltageSource::DC_VoltageSource(std::string component_id, double amplitude, double impedence) : DefaultComponent{component_id}, _V{amplitude}, _Z{impedence} {}
 // Mutator(s)
 void DC_VoltageSource::set_amplitude(double amplitude) { _V = amplitude; }
@@ -17,7 +17,7 @@ double DC_VoltageSource::get_impedence() { return _Z; }
 AC_VoltageSource class definitions
 ------------------------------------------------------------------ */ 
 // Default constructor sets frequency to 50Hz and phase to 0rad
-AC_VoltageSource::AC_VoltageSource() : DefaultComponent{}, DC_VoltageSource{}, _f{50.0}, _p{0.0} {}
+AC_VoltageSource::AC_VoltageSource() : DefaultComponent{}, DC_VoltageSource{}, _f{50.0}, _p{0} {}
 AC_VoltageSource::AC_VoltageSource(std::string component_id, double amplitude, double impedence, double frequency, double phase) : DefaultComponent{component_id}, DC_VoltageSource{component_id, amplitude, impedence}, _f{frequency}, _p{phase} {}
 // Mutator(s)
 void AC_VoltageSource::set_frequency(double frequency) { _f = frequency; }
