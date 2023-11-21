3ecb27b54e80f20a5545d4d1e22d35a3aebac2f5
Harry Brenton <el22hjb@leeds.ac.uk>
2023-11-21 10:52:20 +0000

    removed all old references to ParallelComponent.h

--- dev/Solver.cpp
+++ dev/Solver.cpp
@@ -1,7 +1,6 @@
 // Takes a list of Components from Parser.cpp and models them as a laplace transformed polynomial
 #include <cmath>
 #include "Components.h"
-#include "ParallelComponent.h"
 
 
 int main(int argc, char *argv[]){
@@ -12,7 +11,7 @@ int main(int argc, char *argv[]){
 }
 
 void determine_coefficients(DefaultComponent components[]){
-    
+
 }
 
 double solve_diff_eq(double &a, double &b, double &c, double &vs){
